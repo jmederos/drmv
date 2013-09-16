c7b6fb95434cb277dfb734f82b0cb48b77eba8a3
Jacob Mederos <jacob.mederos@gmail.com>
2013-09-16 15:26:23 -0700

    fixing header

--- main.rb
+++ main.rb
@@ -1,8 +1,6 @@
-#=========="The Stark Secure Server"==================================================================================
-# 	Author: Jacob Mederos																							 #
-# 	Synopsis: This is an IRL implementation of the emergency call system used in Iron Man 3. The idea is to 		 #
-#			  have a single number you can call and pull your contacts, send out a voice/SMS "distress" message		 #
-# 			  to a group of friends/relatives. Other functionalities include a translation system and call recording #
+#=========="DRM Violators"==================================================================================
+# 	Authors: 																							 #
+# 	Synopsis:
 #=====================================================================================================================
 
 require 'sinatra'
