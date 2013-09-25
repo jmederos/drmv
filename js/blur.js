2379dc5ced9c0cdc65657c1b1088cd2336255511
Jacob Mederos <jacob.mederos@gmail.com>
2013-09-24 21:57:04 -0700

    Glass UI works, but...
    only on Chrome after resizing/ calling setGlass()
    
    Major rework of the glass forground/background
    
    Now uses jQuery and scripting to acomplish glass effect vs. using CSS 3
    from my main branch.
    
    Glass buttons work pretty well.

---
+++ js/blur.js
@@ -0,0 +1,28 @@
+function setGlass(){
+	var bg = $("body > .backstretch > img");
+	var blur_bg = $(".newBlur > img");
+
+	var width = bg.css("width");
+	var height = bg.css("height");
+	var top = bg.css("top");
+	var left = bg.css("left");
+
+	console.log(width, height, top, left);
+
+	blur_bg.css("width", width);
+	blur_bg.css("height", height);
+	blur_bg.css( "top", top);
+	blur_bg.css( "left", left);
+	blur_bg.css("position", "fixed");
+	blur_bg.css("z-index", "-100");
+	// blur_bg.css("pointer-events", "none");
+}
+
+function blurMain ()
+{
+	$.backstretch("../images/bg_3.jpg");
+
+	$( window ).resize(function(){
+		setGlass();
+	});
+}
