96ea7e7ad5f759fe44cf0430768caf0d5189618c
Jacob Mederos <jacob.mederos@gmail.com>
2013-09-25 23:50:56 -0700

    Page doesn't glitch on load anymore
    Added javascript to take care of it
    Also a few changes to layout

--- js/blur.js
+++ js/blur.js
@@ -1,7 +1,6 @@
 function setGlass(){
 	var bg = $("body > .backstretch > img");
 	var blur_bg = $(".newBlur > img");
-
 	var width = bg.css("width");
 	var height = bg.css("height");
 	var top = bg.css("top");
@@ -13,16 +12,23 @@ function setGlass(){
 	blur_bg.css("height", height);
 	blur_bg.css( "top", top);
 	blur_bg.css( "left", left);
-	blur_bg.css("position", "fixed");
-	blur_bg.css("z-index", "-100");
-	// blur_bg.css("pointer-events", "none");
+	
 }
 
-function blurMain ()
+function glassInit ()
 {
+	var bg = $("body > .backstretch > img");
+	var blur_bg = $(".newBlur > img");
+
 	$.backstretch("../images/bg_3.jpg");
 
 	$( window ).resize(function(){
 		setGlass();
 	});
+
+	blur_bg.css("position", "fixed");
+	blur_bg.css("z-index", "-100");
+
+	setGlass();
+	setTimeout(setGlass, 80);
 }
